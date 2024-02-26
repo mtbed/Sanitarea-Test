08a2a3348a4c011f87e3886f1b44f2e5daf4ea17
mtbed <bledsolm9773@student.faytechcc.edu>
2024-02-25 22:53:33 -0500

    templates and homepage

--- views.py
+++ views.py
@@ -2,8 +2,13 @@ from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
-# YOOO
+
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", name = "Joe")
+
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name = username)
