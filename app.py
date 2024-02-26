c68b8058a3ccafb626e9e5d41bf0e8b5a238b5c6
mtbed <bledsolm9773@student.faytechcc.edu>
2024-02-25 23:06:54 -0500

    google api stuff

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, session
 from views import views
 from authlib.integrations.flask_client import OAuth
 
-oauth = OAuth(app)
-
 app = Flask(__name__)
+app.secret_key = 'secret'
+
+# oauth config
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='',
+    client_secret='',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid profile email'},
+)
+
+
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
@@ -14,14 +29,16 @@ if __name__ == "__main__":
 
 @app.route('/login')
 def login():
+    google = oauth.create_client('google')
     redirect_uri = url_for('authorize', _external=True)
-    return oauth.twitter.authorize_redirect(redirect_uri)
+    return google.authorize_redirect(redirect_uri)
 
 @app.route('/authorize')
 def authorize():
-    token = oauth.twitter.authorize_access_token()
-    resp = oauth.twitter.get('account/verify_credentials.json')
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
     resp.raise_for_status()
-    profile = resp.json()
+    user_info = resp.json()
     # do something with the token and profile
     return redirect('/')
